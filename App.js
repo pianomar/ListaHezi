eed379a413d0a204b86abc5fd7aaa5d1908cf931
Omar Hezi <omar.hezi@gmail.com>
2022-07-20 18:06:37 -0300

    Add fonts

--- App.js
+++ App.js
@@ -5,16 +5,24 @@ import ToDoMainScreen from './pages/ToDoMainScreen'
 import Colors from './constants/colors'
 import Header from './components/Header'
 import ToDoItemScreen from './pages/ToDoItemScreen';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading'
 
 export default function App() {
 
   const onHandlerClickItem =
     (text) => {
-      setTitle(text)
       setContent(<ToDoItemScreen itemText={text} />)
     }
 
   const [content, setContent] = useState(<ToDoMainScreen onHandlerClickItem={onHandlerClickItem} />);
+
+  const [loaded] = useFonts({
+    SourceSansPro: require('./assets/fonts/SourceSansPro-Regular.ttf'),
+    SourceSansProBold: require('./assets/fonts/SourceSansPro-Bold.ttf')
+  });
+
+  if (!loaded) return <AppLoading />
 
   return (
     <View style={styles.container}>
@@ -28,5 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     marginTop: "10%",
+  },
+  container: {
+    fontFamily: 'SourceSansProBold'
   }
 });
